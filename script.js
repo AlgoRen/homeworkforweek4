9e5cb0172e5095287d1250aee38f5a9b6bba430e
Ren <algorenstudio@gmail.com>
2020-04-16 14:31:59 -0400

    Created the html file strcuture of the quiz, creating a header where the button to view highscores andand count down timer is located. Created a div that works as the starting page when user first loads onto site with text and a button the turns that div invisible and makes visible a div called questionPage that is used to display the question and answer portion of the skate quiz. Created the array of objects that hold questions, possible answers, and correct answer for each page of questions. Created a function that appeneds the correct question depending on page. Created a function that appends the correct answer buttons depending on page. Created a function that create and displays countdown timer to page.

--- script.js
+++ script.js
@@ -49,3 +49,88 @@
             // take score and initial
             // You'll save that score, and the initials to localStorage
 
+var timeCount = document.querySelector("#timeCount");
+var startQuiz = document.querySelector("#startQuiz");
+var welcomePage = document.querySelector("#startingPage");
+var quizPage = document.querySelector("#questionPage");
+var secondsLeft = 100;
+
+var displayQuestion = document.querySelector(".question");
+var displayAnswers = document.querySelector(".answers")
+var page = 0;
+var content = 0;
+
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeCount.textContent = secondsLeft + " seconds.";
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage(); //change when ready to game over screen
+    }
+
+  }, 1000);
+}
+
+function moveToNext(){
+    for (var m = 0; m < quizContent.length; m++) {
+        var j = 0;
+        j++;
+    }
+}
+
+function createAnswerButtons(){
+    for (var i = 0; i < 4; i++){
+        answerBtn = document.createElement("Button")
+        answerBtn.textContent = quizContent[page].answers[i]
+        displayQuestion.appendChild(answerBtn);
+    }
+}
+
+function goThroughQuiz(){
+    startQuiz.addEventListener("click", function(){
+        welcomePage.parentNode.removeChild(welcomePage);
+        quizPage.setAttribute("class", "visible w-50 mx-auto");
+        displayQuestion.children[0].textContent = quizContent[page].question
+        createAnswerButtons();
+        setTime();
+
+        console.log(quizContent[page].answers[0]);
+        console.log(quizContent[page].answers[1]);
+        console.log("----------------------------");
+        console.log(quizContent[page + 1].question);
+        console.log(quizContent[page + 1].answers[0]);
+    })
+}
+
+function newPage(){
+
+}
+
+var quizContent = [
+    {
+        question: "first question",
+        answers: ["answer 1", "answer 2", "answer 3", "answer 4"],
+        correctAnswer: "this answer"
+    }, 
+    {
+        question: "second question",
+        answers: ["answer 13", "answer 12", "answer 13", "answer 14"],
+        correctAnswer: "this answer"
+    },
+    {
+        question: "third question",
+        answers: ["answer 21", "answer 22", "answer 23", "answer 24"],
+        correctAnswer: "this answer"
+    },
+    {
+        question: "fourth question",
+        answers: ["answer 31", "answer 32", "answer 33", "answer 34"],
+        correctAnswer: "this answer"
+    }
+]
+
+
+goThroughQuiz();
